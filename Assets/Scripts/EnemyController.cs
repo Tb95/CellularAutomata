4eaebcba6ed8e98fc24442339b9453b944e3ab37
Tommaso Bianchi <tommaso.bianchi95@gmail.com>
2016-01-11 20:00:27 +0100

    Skeleton enemy added (asset + simple script + animations)

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+    [Range(1, 100)]
+    public float speed;
+    [Range(1, 100)]
+    public float rotationSpeed;
+
+    Transform target;
+    Rigidbody myRigidbody;
+    Vector3 velocity;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        velocity = (target.position - transform.position).normalized * speed;
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(velocity), rotationSpeed);
+    }
+
+    void FixedUpdate()
+    {
+        myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+}
